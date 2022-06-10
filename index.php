01a744046a560735a0f777e227287700e50ccca6
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-06-10 16:58:00 +0200

    primo milestone

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+    $database = [
+        [
+            'title' => 'New Jersey',
+            'author' => 'Bon Jovi',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+            'genre' => 'Rock'
+        ],
+        [
+            'title' => 'Live at Wembley 86',
+            'author' => 'Queen',
+            'year' => 1992,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Ten\'s Summoner\'s Tales',
+            'author' => 'Sting',
+            'year' => 1993,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Steve Gadd band',
+            'author' => 'Steve Gadd Band',
+            'year' => 2018,
+            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+            'genre' => 'Jazz'
+        ],
+        [
+            'title' => 'Brave new World',
+            'author' => 'Iron Maiden',
+            'year' => 2000,
+            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+            'genre' => 'Metal'
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+            'genre' => 'Rock'
+        ]
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <header>
+
+    </header>
+
+    <main>
+        <div class="ms_container row row-cols-4 mt-4">
+            <?php foreach ($database as $disc) { ?>
+            <div class="card col p-3 text-center">
+                    <img src="<?php echo $disc["poster"]; ?>" alt="">  
+                    <h2 class="text-uppercase"> <?php echo $disc["title"]; ?> </h2>
+                    <small> <?php echo $disc["author"]; ?> </small>
+                    <small> <?php echo $disc["year"]; ?> </small>
+                    
+                </div>
+            <?php } ?>
+            
+        </div>
+    </main>
+
+</body>
+</html>
